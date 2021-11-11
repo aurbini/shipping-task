fd9b6b33407f722fa2f50a57a74b5c2a83a09c45
jarrilla <jarrilla.services@gmail.com>
2021-11-11 16:20:21 -0500

    added starter code

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+const { randomInt } = require('crypto');
 const { json, urlencoded } = require('express');
 const express = require('express');
+const { model, Schema, connect } = require('mongoose');
+const PackageModel = require('./models/Package');
+const ShipmentModel = require('./models/Shipment');
+require('dotenv').config();
 
 const app = express();
 app.use( json() );
@@ -8,5 +13,111 @@ app.use( urlencoded({ extended: true }) );
 app.use('/packages', require('./controllers/packages'));
 app.use('/shipments', require('./controllers/shipments'));
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('API running on ' + port));
+app.post('/reset', resetAll);
+
+// ---------------------------
+// Though it is bad practice to do so, the starter code provided below
+// is here to avoid cluttering the rest of your project.
+// ---------------------------
+
+const OrderModel = model('orders', new Schema({
+  customerId: String,
+  orderNumber: String,
+  items: [{
+    part: String,
+    batch: Number,
+    qty: Number,
+  }]
+}));
+
+
+/**
+ * Drop all collections & populate orders with semi-random data.
+ */
+async function resetAll(_req, res) {
+
+  console.log('Resetting collections...');
+
+  const _dropCollection = async model => {
+    try {
+      await model.collection.drop();
+      return true;
+    }
+    catch (e) {
+      // collection doesn't exist; ok
+      if ( e.name === 'MongoServerError' && e.code === 26 ) {
+        return true;
+      }
+      else {
+        console.error(e);
+        return false;
+      }
+    }
+  };
+
+  try {
+    const ok = _dropCollection( OrderModel ) &&
+      _dropCollection( PackageModel ) &&
+      _dropCollection( ShipmentModel );
+
+    if (!ok) return res.status(500).send('Error dropping collections.');
+
+    const promises = [];
+    ['ABC', 'BCA', 'CAB'].map(customerId => {
+      for (let i = 1; i <= randomInt(1, 10); i++) {
+        const orderNumber = 1000 + i;
+
+        const items = [];
+        for (let j = 0; j <= randomInt(1, 4); j++) {
+          const newItem = {
+            part: `PN-0${j+1}`,
+            batch: 1,
+            qty: randomInt(1, 10)
+          };
+
+          items.push(newItem);
+        }
+
+        const newOrder = new OrderModel({
+          customerId,
+          orderNumber,
+          items
+        });
+
+        promises.push( newOrder.save() );
+      }
+    });
+
+    await Promise.all(promises);
+
+    console.log('Collections reset!');
+
+    res.sendStatus(200);
+  }
+  catch (e) {
+    console.error(e);
+    res.status(500).send('Error resetting models.');
+  }
+
+};
+
+// connect to DB & listen
+(async () => {
+
+  try {
+    await connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => console.log('API running on ' + port));
+  }
+  catch (e) {
+    console.error(e);
+    console.log('Failed to connect to DB. Exiting...');
+
+    process.exit();
+  }
+})();
