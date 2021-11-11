94d1b0516e5fbb7ed070bac51fca71e48e2d667e
jarrilla <jarrilla.services@gmail.com>
2021-11-11 16:21:07 -0500

    fixed typo

--- models/Package.js
+++ models/Package.js
@@ -8,5 +8,5 @@ const PackageSchema = new mongoose.Schema({
   // TODO: Complete this according the the specifications outlined in the README
 });
  
-const PackageModel = mongoose.model('shipment', PackageSchema);
+const PackageModel = mongoose.model('package', PackageSchema);
 module.exports = PackageModel;
